3988731181ae10ca4e3bf5f939404738a2b7c938
j-burlison <josh1burlison@gmail.com>
2014-09-18 12:43:10 -0500

    Fixed responsive nature and mobile tweaking. Also added floating scroll menu

--- MobileCssTweak.js
+++ MobileCssTweak.js
@@ -12,6 +12,29 @@ $(function() {
 		$("#gittext").css("margin-left", "38%");
 		$("#emailtext").css("margin-left", "25%");
 		$(".tab-content").css("border-right", "0px");
+		$("#contact_button").css("margin-left", "32%");
+		$("#collapseOneExpand").css("display", "none");
+		$("#collapseTwoExpand").css("display", "none");
+		$("#uShip").css("display", "none");
+		$("#uShip_mobile").css("visibility", "visible");
+		$("#uShip_mobile").css("width", "60%");
+		$("#uShip_mobile").css("margin-left", "15%");
+		$("#THD").css("display", "none");
+		$("#THD_mobile").css("visibility", "visible");
+		$("#THD_mobile").css("width", "60%");
+		$("#THD_mobile").css("margin-left", "15%");	
+		$("#tswcomments").css("margin-left", "0%");	
+		$("#tswcomments").css("width", "80%");
+		$("#sendFeedback").css("margin-left", "55%");	
+		$(".thankYouModal").css("width", "80%");	
+		$("#contactFormModal").css("position", "absolute");	
+			
+		$(".thankYouOops").css("font-size", "30px");
+		$(".thankYouOops").css("text-shadow", "none");
+		
+		$("#collapseOne").removeClass("collapse");	
+		$("#collapseTwo").removeClass("collapse");	
+		
    }
    
    else{
--- about.js
+++ about.js
@@ -1,17 +1,43 @@
 jQuery(document).ready(function() {
     var offset = 220;
     var duration = 500;
+    
+    jQuery('#to-middle').fadeIn(duration);
+    jQuery('#to-bottom').fadeIn(duration);
+            
     jQuery(window).scroll(function() {
         if (jQuery(this).scrollTop() > offset) {
-            jQuery('.back-to-top').fadeIn(duration);
-        } else {
-            jQuery('.back-to-top').fadeOut(duration);
+            jQuery('#back-to-top').fadeIn(duration);
+        } 
+        
+        else if (jQuery(this).scrollTop() <= offset){
+            jQuery('#back-to-top').fadeOut(duration);
         }
+        
+        if (jQuery(this).scrollTop() < $(document).height()-$(window).height()) {
+            jQuery('#to-bottom').fadeIn(duration);
+        } 
+        
+        else if (jQuery(this).scrollTop() >= $(document).height()-$(window).height()) {
+            jQuery('#to-bottom').fadeOut(duration);
+        } 
     });
     
-    jQuery('.back-to-top').click(function(event) {
+    jQuery('#back-to-top').click(function(event) {
         event.preventDefault();
         jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+    
+    jQuery('#to-middle').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: $(document).height() / 2}, duration);
+        return false;
+    })
+    
+    jQuery('#to-bottom').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: $(document).height()-$(window).height()}, duration);
         return false;
     })
 });
@@ -20,7 +46,7 @@ jQuery(document).ready(function() {
 
 
 
-$(function() {
+jQuery(document).ready(function() {
 	
 	if (typeof InstallTrigger !== 'undefined') {
 		$("#tswcomments").css("width", "70%");
@@ -34,43 +60,7 @@ $(function() {
 		$("#oopsModal").modal('show');
 	}
 	
-   
-	//$("#sendFeedback").mousedown(function() {
-		//$("#contactForm").css('display', 'none');
-		//$("#thankYou").css('display', 'block');
-		
-   //});   
-   
-   $("#myFaceModal").mousedown(function() {
-		$("#myFaceModal").modal('hide');
-   });
-   
-   $("#HalfDomeSelfieModal").mousedown(function() {
-		$("#HalfDomeSelfieModal").modal('hide');
-   });
-   
-   $("#CostaRicaModal").mousedown(function() {
-		$("#CostaRicaModal").modal('hide');
-   });
-   
-   $("#YosemiteHorModal").mousedown(function() {
-		$("#YosemiteHorModal").modal('hide');
-   });
-   
-   $("#ParaJumpModal").mousedown(function() {
-		$("#ParaJumpModal").modal('hide');
-   });
-   
-   $("#MotoClassModal").mousedown(function() {
-		$("#MotoClassModal").modal('hide');
-   });
-   
-   $("#MyFamilyModal").mousedown(function() {
-		$("#MyFamilyModal").modal('hide');
-   });
-   
-   
-   $("#collapseOneExpand").mousedown(function() {
+	$("#collapseOneExpand").mousedown(function() {
    	if ($('#collapseOneText').text() == "Click To Expand Section"){
    		$('#collapseOneText').text("Click To Collapse Section");
    	}
@@ -89,6 +79,100 @@ $(function() {
 			$('#collapseTwoText').text("Click To Expand Section");   	
    	}
    });
+	
+     
+   $("#myFace").mousedown(function() {
+		jQuery('html, body').animate({scrollTop: $("#myFaceModal").scrollTop()}, 500);   
+   });
+   
+   
+   $("#myFaceModal").mousedown(function() {
+		$("#myFaceModal").modal('hide');
+   });
+   
+
+   
+   $("#HalfDomeSelfie").mousedown(function() {
+		jQuery('html, body').animate({scrollTop: $("#HalfDomeSelfieModal").scrollTop()}, 500);   
+   });
+   
+ 
+   
+   $("#HalfDomeSelfieModal").mousedown(function() {
+		$("#HalfDomeSelfieModal").modal('hide');
+   });
+   
+
+   
+   
+   
+   $("#CostaRica").mousedown(function() {
+		jQuery('html, body').animate({scrollTop: $("#CostaRicaModal").scrollTop()}, 500);   
+   });
+
+   
+   $("#CostaRicaModal").mousedown(function() {
+		$("#CostaRicaModal").modal('hide');
+   });
+   
+
+   
+   
+   
+   $("#YosemiteHor").mousedown(function() {
+		jQuery('html, body').animate({scrollTop: $("#YosemiteHorModal").scrollTop()}, 500);   
+   });
+   
+
+   
+   $("#YosemiteHorModal").mousedown(function() {
+		$("#YosemiteHorModal").modal('hide');
+   });
+   
+
+   
+   
+   
+   $("#ParaJump").mousedown(function() {
+		jQuery('html, body').animate({scrollTop: $("#ParaJumpModal").scrollTop()}, 500);   
+   });
+   
+
+   $("#ParaJumpModal").mousedown(function() {
+		$("#ParaJumpModal").modal('hide');
+   });
+   
+ 
+   
+   
+   $("#MotoClass").mousedown(function() {
+		jQuery('html, body').animate({scrollTop: $("#MotoClassModal").scrollTop()}, 500);   
+   });
+   
+ 
+   
+   $("#MotoClassModal").mousedown(function() {
+		$("#MotoClassModal").modal('hide');
+   });
+   
+
+   
+   
+   
+   $("#MyFamily").mousedown(function() {
+		jQuery('html, body').animate({scrollTop: $("#MyFamilyModal").scrollTop()}, 500);   
+   });
+   
+ 
+   
+   $("#MyFamilyModal").mousedown(function() {
+		$("#MyFamilyModal").modal('hide');
+   });
+   
+ 
+   
+   
+   
 
 
 });
